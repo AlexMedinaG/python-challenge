04a593adce55ba671f02f0b5198a3c4a6fd15e69
AlexMedinaG <medinagomez.alejandro@gmail.com>
2023-04-17 22:19:50 -0600

    a

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,9 +2,11 @@
 
 import csv
 
+#open file as csv
 with open('./Resources/budget_data.csv', encoding='utf-8') as file:
     csv_reader = csv.reader(file)
 
+# declare starting conditions
     months = []
     profit = []
     change = 0
@@ -14,12 +16,17 @@ with open('./Resources/budget_data.csv', encoding='utf-8') as file:
     decrease = 0
     decrease_date = 0
 
+#skip headers
     next(csv_reader)
 
+
     for row in csv_reader:
+
+#for each row create a list for months and profit
         months.append(row[0]) 
         profit.append(int(row[1]))
 
+#for each row, determine if the value is higher or lower than the "change" value; if so, save the values in row 1 and 0 to each variable
         if int(row[1]) - change > increase:
             increase = int(row[1]) - change
             increase_date = row[0]
@@ -27,19 +34,27 @@ with open('./Resources/budget_data.csv', encoding='utf-8') as file:
             decrease = int(row[1]) - change
             decrease_date = row[0]
 
+#makes a list with the values for change
         change_list.append(int(row[1]) - change)
-
         change = int(row[1])
 
+#takes out the first item from the list 
 change_list.pop(0)
+
+#print information to terminal
 print('Financial Analysis')
 print('----------------------------')
+#count the list items to get the total months
 print('Total Months: ' + str(len(months)))
+#sum all profits to get the total 
 print('Total: $' + str(sum(profit)))
+#round the sum of all the items from the change_list and divide by the count of the items to get the average
 print('Average change: $' + str(round(sum(change_list)/len(change_list), 2)))
+#get the latest value for the variables in increase and decrease profits
 print('Greatest Increase in Profits: ' + increase_date + ' ($' + str(increase) + ')')
 print('Greatest Decrease in Profits: ' + decrease_date + ' ($' + str(decrease) + ')')
 
+#write information to text file
 with open('./analysis/PyBank.txt', 'w') as file:
     file.write('Financial Analysis' + '\n')
     file.write('----------------------------' + '\n')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,16 +2,20 @@
 
 import csv
 
+#open file as csv
 with open('./Resources/election_data.csv', encoding='utf-8') as file:
     csv_reader = csv.reader(file)
 
+# declare starting conditions
     ballot = []
     charles = 0
     diana = 0
     raymon = 0
 
+#skip headers
     next(csv_reader)
 
+# for each row, count every time a name equals a different candidate
     for row in csv_reader:
         ballot.append(row[0]) 
         
@@ -22,23 +26,30 @@ with open('./Resources/election_data.csv', encoding='utf-8') as file:
         if row[2] == 'Raymon Anthony Doane':
             raymon += 1
 
+#array for determining winner
 candidates = {
     'Charles Casper Stockham': charles,
     'Diana DeGette': diana,
     'Raymon Anthony Doane': raymon
 }
 
+#print information to terminal
 print('Election Results')
 print('-------------------------')
+#using len to count the list size
 print('Total Votes: ' + str(len(ballot)))
 print('-------------------------')
-print('Charles Casper Stockham: ' + str(round(100*charles/len(ballot), 3)) + '% (' + str(charles) + ')')
+#takes the candidate and give the % by dividing the number of times the candidate name repeats by the total ballots
+print('Diana DeGette: ' + str(round(100*diana/len(ballot), 3)) + '% (' + str(diana) + ')')
+print('Charles Casper Stockham: ' + str(round(100*charles/len(ballot), 3)) + '% (' + str(charles) + ')') 
 print('Diana DeGette: ' + str(round(100*diana/len(ballot), 3)) + '% (' + str(diana) + ')')
 print('Raymon Anthony Doane: ' + str(round(100*raymon/len(ballot), 3)) + '% (' + str(raymon) + ')')
 print('-------------------------')
-print('Winner: ' + str(max(candidates, key=candidates.get)))
+#get the candidate with the max number of ballots, from candidates array
+print('Winner: ' + str(max(candidates, key=candidates.get))) 
 print('-------------------------')
 
+#write information to text file
 with open('./analysis/PyPoll.txt', 'w') as file:
     file.write('Election Results' + '\n')
     file.write('-------------------------' + '\n')
